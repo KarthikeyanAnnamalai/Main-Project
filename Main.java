999daa088affb031524aa4e37d294024597868d0
Karthikeyan Annamalai <karthikit0312@gmail.com>
2020-12-17 18:49:42 +1300

    Storing File

---
+++ Main.java
@@ -0,0 +1,22 @@
+package com.example.java;
+
+import java.math.BigDecimal;
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hi Karthik");
+	String aName = "Karunakaran";
+        System.out.println("My Name is " + aName);
+
+        double value = 0.015;
+        double psum = value + value + value;
+        System.out.println("The String Value " + psum);
+        String strvalue = Double.toString(value);
+        System.out.println("The String Value " + strvalue);
+
+     
+
+    }
+}
